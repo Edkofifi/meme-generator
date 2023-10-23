dc68e5c3e06e9190aff2ab26059d4116fed023c9
Edkofifi <atk.eddy@gmail.com>
2023-10-23 12:55:24 +0000

    removed notes.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import './App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
 
-// running the notes components here
-import Notes from './components/Notes';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Meme/>
-      <Notes/>
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Contact(){
+    const [contact, setContact] = React.useState({
+        firstName: "Nikeh",
+        lastName: "Teye",
+        phone: "+233 (244) 586-922",
+        email: "myemail@gmai.com",
+        isFavourite: true
+    })
+    return (
+
+
+
+        <main>
+            <article>
+                <img src="" />
+                <div>
+                    <img src="" />
+                </div>
+            </article>
+        </main>
+    )
+}
